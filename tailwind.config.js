1fa8a1c663c158cf92512ae87a0f172c8f992be3
GodPFM <asmeev.daniil@yandex.ru>
2022-12-16 16:13:33 +0500

    feat: add font families for custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ['./src/**/*.{html,js}'],
     theme: {
         extend: {},
+        fontFamily: {
+            'HelveticaNeueCyr': ['HelveticaNeueCyr', 'Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+            'Arial': ['Arial', 'Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+        },
     },
     plugins: [],
 }
