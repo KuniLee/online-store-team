bc5eda8cf49bd03a49ab1984e3292aec824ea710
Yuriy Sitnikov <selnetys@gmail.com>
2022-12-21 12:51:57 +0500

    feat: mvc directories

---
+++ src/components/models/model.ts
@@ -0,0 +1,16 @@
+// import EventEmitter from 'events'
+//
+// type Item = {
+//     id: string
+//     name: string
+// }
+//
+// type ItemModelEventsName = 'ITEM_ADD' | 'ITEM_REMOVE'
+// export type ItemsModelInstance = InstanceType<typeof ItemsModel>
+//
+// export class ItemsModel extends EventEmitter {
+//     constructor(items?: Item[]) {
+//         super()
+//         this.items = items || []
+//     }
+// }
