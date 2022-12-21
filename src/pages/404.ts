9781b200583815321af6911a5c2c6d0a45c29593
Yuriy Sitnikov <selnetys@gmail.com>
2022-12-21 15:16:13 +0500

    feat: remove 404 page

--- src/pages/404.ts
+++ src/pages/404.ts
@@ -1,7 +1,24 @@
+import footer from '@/templates/footer.html'
+import header from '@/templates/header.html'
+
 export function NotFound() {
-    const div = document.createElement('template')
-    const h1 = document.createElement('h1')
-    h1.innerText = 'Not found'
-    div.append(h1)
-    return div.content
+    const fragment = document.createElement('template')
+    const fragmentFooter = document.createElement('template')
+    const fragmentHeader = document.createElement('template')
+
+    // const Model = new ItemsModel()
+    // const View = new ItemsView(Model, container)
+    // const Controller = new ItemsController(Model, View)
+
+    fragment.innerHTML = ` <main class="flex-auto flex-shrink-0">
+    <div class="max-w-[1200px] mx-auto flex justify-center items-center">
+      <p class="text-[120px]">404</p>
+    </div>
+  </main>`
+    fragmentFooter.innerHTML = footer
+    fragmentHeader.innerHTML = header
+    fragment.content.append(fragmentFooter.content)
+    fragment.content.prepend(fragmentHeader.content)
+
+    return fragment.content
 }
