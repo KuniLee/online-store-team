542371769aa09595a22986b7284f558efd56b0d8
GodPFM <asmeev.daniil@yandex.ru>
2023-01-14 15:40:21 +0500

    feat: add test of find article in local storage

--- Tests/test.test.ts
+++ Tests/test.test.ts
@@ -1,6 +1,49 @@
 import { CatalogView } from '@/components/views/CatalogView'
 import { Item } from 'types/interfaces'
+import {AppModel} from "@/components/models/model";
+import {AppView} from "@/components/views/AppView";
 
-test('My first test', () => {
-    expect(Math.max(1, 5, 10)).toBe(10)
+class LocalStorageMock {
+    private cartArticles: string;
+    length: number;
+    constructor() {
+        this.cartArticles = '[{"article":1295042,"count":1},{"article":1294716,"count":1},{"article":1297914,"count":1},{"article":1295039,"count":1}]'
+        this.length = 0
+    }
+
+    getItem(value: string) {
+        switch (value) {
+            case 'cartArticles':
+                return JSON.stringify(this.cartArticles)
+            case 'sumOfCart':
+                return '123'
+        }
+        return null
+    }
+
+    setItem(value: string) {
+        this.cartArticles = value
+    }
+
+    clear() {
+
+        this.cartArticles = '[]'
+    }
+
+    key() {
+        return '1' || null
+    }
+
+    removeItem() {
+        return
+    }
+}
+
+test('Find article in localStorage', () => {
+    const model = new AppModel()
+    global.localStorage = new LocalStorageMock;
+    expect(model.checkItemInCart(1295042)).toBe(true)
+    expect(model.checkItemInCart(129504)).toBe(false)
+    localStorage.clear()
+    expect(model.checkItemInCart(1295042)).toBe(false)
 })
